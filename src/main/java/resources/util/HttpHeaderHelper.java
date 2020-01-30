094574e17081e4b9e44cd76fdd46b65cdc721223
Dragos Stefanescu <dragos.nicolae.stefanescu@gmail.com>
2020-01-30 13:53:17 +0200

    refactor code

--- src/main/java/resources/util/HttpHeaderHelper.java
+++ src/main/java/resources/util/HttpHeaderHelper.java
@@ -2,17 +2,11 @@ package resources.util;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-
-import java.util.Base64;
 
 @Component
 public class HttpHeaderHelper {
-    private static final String AUTHORIZATION = "Authorization";
-    private static final String BASIC = "Basic ";
-    private static final String CONTENT = "Content-Type";
-    private static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
-
     @Value("${authorizationServer.clientId:clientIdPassword}")
     private String clientId;
 
@@ -21,7 +15,7 @@ public class HttpHeaderHelper {
 
     public HttpHeaders getLoginHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add(CONTENT, CONTENT_TYPE);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.setBasicAuth(clientId, clientSecret);
 
         return headers;
